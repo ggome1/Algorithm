d2bd8d7eb1e3d0d038861415eb7d144174b1f391
ggome1 <bookincar@naver.com>
2024-12-13 19:59:57 +0900

    11651 solved

---
+++ sort/11651.js
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../input.txt').toString().trim().split("\n").map(element => element.trim().split(" ").map(Number));
+
+const length = input.shift();
+input.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    return a[1] - b[1];
+});
+console.log(input.map(element => element.join(" ")).join("\n"));
