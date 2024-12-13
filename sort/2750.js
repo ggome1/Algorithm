a115c731046594ed21e317995414f56bab692022
ggome1 <bookincar@naver.com>
2024-12-13 17:34:07 +0900

    2750 solved

---
+++ sort/2750.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../input.txt').toString().trim().split("\n").map(Number);
+const length = input.shift();
+const no_repeat = new Set(input);
+const result = [...no_repeat].sort((a, b) => a - b);
+for (num of result) {
+    console.log(num);
+}
