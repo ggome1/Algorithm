42532c587be743b1d90c522174d307bbbc96edae
ggome1 <bookincar@naver.com>
2026-01-07 22:17:31 +0900

    chore: 폴더 구조 변경

---
+++ Python/20291.py
@@ -0,0 +1,17 @@
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(input().rsplit(".", 1)[-1])
+
+answer = {}
+
+for i in arr:
+    if i in answer:
+        answer[i] += 1
+    else:
+        answer[i] = 1
+
+for k, v in sorted(answer.items()):
+    print(k, v)
